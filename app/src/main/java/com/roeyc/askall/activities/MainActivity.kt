e56fccc9a15d2d9b239e5e0073a682fa9800a1ab
roeyc <roeyc@gett.com>
2021-01-20 13:40:25 +0200

    adding firbase adding firebase authentication

--- app/src/main/java/com/roeyc/askall/activities/MainActivity.kt
+++ app/src/main/java/com/roeyc/askall/activities/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.roeyc.askall.NavigationIconClickListener
 import com.roeyc.askall.R
 import com.roeyc.askall.databinding.ActivityMainBinding
@@ -17,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     // using android  https://developer.android.com/topic/libraries/view-binding
     private lateinit var binding: ActivityMainBinding
     lateinit var viewModel: MainActivityViewModel
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +37,59 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         setMenu()
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.loginButton.setOnClickListener() {
+            signIn()
+        }
+
+        binding.logoutButton.setOnClickListener()
+        {
+            signOut()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            binding.hello.text = getString(R.string.sign_in_signed_user, user.displayName) //"Welcome: " + user.displayName
+        } else {
+            binding.hello.setText(R.string.sign_in)
+        }
+    }
+
+    private fun signIn() {
+        // with firebaseui
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setIsSmartLockEnabled(false)
+            .setTheme(R.style.LoginTheme)
+            .build(),
+            1822
+        )
+
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                Toast.makeText(this,"signed out successfully" , Toast.LENGTH_LONG).show()
+            }
+        updateUI(null)
     }
 
     private fun setMenu() {
